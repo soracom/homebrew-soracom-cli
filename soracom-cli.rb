04b251ef218d4accd08542c65d357e75a769e940
Takashi Oguma <oguma@ogumac2015.local>
2016-11-30 10:48:02 +0900

    bump version

--- soracom-cli.rb
+++ soracom-cli.rb
@@ -1,7 +1,7 @@
 class SoracomCli < Formula
-  VERSION = "0.1.5"
-  SHA256_64 = "e567e826cfe825a9b84614cc16b59ad7ad05342ca556c2ea7c94df88d9f1d670"
-  SHA256_32 = "b5cb1ad680c1c6e28b2a7360d7fd4fbc7012354f800525e71e92022fa0446f98"
+  VERSION = "0.1.7"
+  SHA256_64 = "e7eba9a93884e0aa1619eb028060f3b081f9e528a265d892fe85e845e7b7bf58"
+  SHA256_32 = "cc6dfe04612ea76d7bda13c60c74ecef5e30113b821b5d7aab1a42156c1d4540"
 
   desc "A command line tool to invoke SORACOM API"
   homepage "https://github.com/soracom/soracom-cli"
