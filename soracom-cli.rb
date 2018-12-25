5e1e2c041a32a4081857341b6d2cef2385d530e5
bearmini <oguma@soracom.jp>
2018-12-25 15:58:31 +0900

    bump version

--- soracom-cli.rb
+++ soracom-cli.rb
@@ -1,7 +1,7 @@
 class SoracomCli < Formula
-  VERSION = "0.3.9"
-  SHA256_64 = "4bae5b48feaed90786dd91984e52234865befb653de8bd39d4a8562ad3fae3c7"
-  SHA256_32 = "f7b8d3268111f525a29e3ca198c226428ad5b1e36f22e6db39e9eccd5278fd8a"
+  VERSION = "0.4.0"
+  SHA256_64 = "b92e96684bcb345f0f5bac88d3598676ff7d261281cebc3d4b4811cbcbb5cd5a"
+  SHA256_32 = "f0e4f199f70766a4786c1b6447267ae215c9500f1a4215f0398265c803d50bda"
 
   desc "A command line tool to invoke SORACOM API"
   homepage "https://github.com/soracom/soracom-cli"
