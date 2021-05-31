360e1c51a188c16edf145236cafd53b864e46782
Takashi Oguma <oguma@soracom.jp>
2021-05-31 00:40:11 +0000

    update formula to support arm64

--- soracom-cli.rb
+++ soracom-cli.rb
@@ -1,18 +1,22 @@
 class SoracomCli < Formula
-  VERSION = "0.5.1"
-  SHA256_MAC_32 = "190b3f65ad434f31209ee9a973d7481cdb97431966a82321fff425298786fb9a"
-  SHA256_MAC_64 = "c765c886b45d5e4e1857b330ef838fd6edeff0f12dbdfc614d5e0b1eb524d096"
-  SHA256_LINUX_32 = "34ccd74ca83e611feaa71808e428cc0db5fb09f4caaf0a5c69a2feb0e9c58bcc"
-  SHA256_LINUX_64 = "2a28f0f3767b256f4b5aaefc21d835fc77678a103a77ea0b3a4caddc0ccec043"
+  VERSION = "0.8.0"
+  SHA256_MAC_amd64 = "090405cf5310e6b1c8f3744dffc6684afbfec8323b82e5e32e9c73f051eb480f"
+  SHA256_MAC_arm64 = "1f8f50b98d897e2e610a70feb006f8e050f668b81b1919dfab19cfc448e46783"
+  SHA256_LINUX_amd64 = "658e5c1f11e7ec666e3a66c252d21e81281a1798cba519189f7bafe56ee08c99"
+  SHA256_LINUX_arm64 = "bcc2019990a478d646aeb0108d6255a2f1c435422c4c7005cd3dfa508c814a31"
+  SHA256_LINUX_386 = "84be22e5d0bb76818cc4a5511a4b1d85ee1ead121fa9b716cb2de735504af2b1"
+  SHA256_LINUX_arm = "35856cbb2025aca01c8a69fa9f7f298079272d0eab81b53cfc954971ea5ad6e4"
 
   SHA256 = {
     'darwin' => {
-      'amd64' => SHA256_MAC_64,
-      '386'   => SHA256_MAC_32,
+      'amd64' => SHA256_MAC_amd64,
+      'arm64' => SHA256_MAC_arm64,
     },
     'linux' => {
-      'amd64' => SHA256_LINUX_64,
-      '386'   => SHA256_LINUX_32,
+      'amd64' => SHA256_LINUX_amd64,
+      'arm64' => SHA256_LINUX_arm64,
+      '386'   => SHA256_LINUX_386,
+      'arm'   => SHA256_LINUX_arm,
     },
   }
 
@@ -26,10 +30,15 @@ class SoracomCli < Formula
     os = 'linux'
   end
 
-  if Hardware::CPU.is_64_bit?
+  case `uname -m`
+  when 'x86_64'
     arch = 'amd64'
-  else
+  when 'arm64', 'aarch64'
+    arch = 'arm64'
+  when 'i686'
     arch = '386'
+  when 'armv6l', 'armv7l'
+    arch = 'arm'
   end
 
   @@binname = "soracom_#{VERSION}_#{os}_#{arch}"
--- update-formula.sh
+++ update-formula.sh
@@ -6,16 +6,22 @@ if [ "$VERSION" == "" ]; then
     exit 1
 fi
 
+set -Eeuo pipefail
+
 : "Calculate checksum" && {
-    SHA256_MAC_32=$(curl -s -L "https://github.com/soracom/soracom-cli/releases/download/v$VERSION/soracom_${VERSION}_darwin_386"   | shasum -a 256 | cut -f 1 -d ' ' )
-    SHA256_MAC_64=$(curl -s -L "https://github.com/soracom/soracom-cli/releases/download/v$VERSION/soracom_${VERSION}_darwin_amd64" | shasum -a 256 | cut -f 1 -d ' ' )
-    SHA256_LINUX_32=$(curl -s -L "https://github.com/soracom/soracom-cli/releases/download/v$VERSION/soracom_${VERSION}_linux_386"   | shasum -a 256 | cut -f 1 -d ' ' )
-    SHA256_LINUX_64=$(curl -s -L "https://github.com/soracom/soracom-cli/releases/download/v$VERSION/soracom_${VERSION}_linux_amd64" | shasum -a 256 | cut -f 1 -d ' ' )
+    SHA256_MAC_amd64=$(curl -s -L "https://github.com/soracom/soracom-cli/releases/download/v$VERSION/soracom_${VERSION}_darwin_amd64" | shasum -a 256 | cut -f 1 -d ' ' )
+    SHA256_MAC_arm64=$(curl -s -L "https://github.com/soracom/soracom-cli/releases/download/v$VERSION/soracom_${VERSION}_darwin_arm64"   | shasum -a 256 | cut -f 1 -d ' ' )
+    SHA256_LINUX_amd64=$(curl -s -L "https://github.com/soracom/soracom-cli/releases/download/v$VERSION/soracom_${VERSION}_linux_amd64" | shasum -a 256 | cut -f 1 -d ' ' )
+    SHA256_LINUX_arm64=$(curl -s -L "https://github.com/soracom/soracom-cli/releases/download/v$VERSION/soracom_${VERSION}_linux_arm64" | shasum -a 256 | cut -f 1 -d ' ' )
+    SHA256_LINUX_386=$(curl -s -L "https://github.com/soracom/soracom-cli/releases/download/v$VERSION/soracom_${VERSION}_linux_386"   | shasum -a 256 | cut -f 1 -d ' ' )
+    SHA256_LINUX_arm=$(curl -s -L "https://github.com/soracom/soracom-cli/releases/download/v$VERSION/soracom_${VERSION}_linux_arm"   | shasum -a 256 | cut -f 1 -d ' ' )
 }
 
 sed -e "s/VERSION *=.*/VERSION = \"$VERSION\"/g"       -i'.bak' soracom-cli.rb
-sed -e "s/SHA256_MAC_32 *=.*/SHA256_MAC_32 = \"$SHA256_MAC_32\"/g" -i'.bak' soracom-cli.rb
-sed -e "s/SHA256_MAC_64 *=.*/SHA256_MAC_64 = \"$SHA256_MAC_64\"/g" -i'.bak' soracom-cli.rb
-sed -e "s/SHA256_LINUX_32 *=.*/SHA256_LINUX_32 = \"$SHA256_LINUX_32\"/g" -i'.bak' soracom-cli.rb
-sed -e "s/SHA256_LINUX_64 *=.*/SHA256_LINUX_64 = \"$SHA256_LINUX_64\"/g" -i'.bak' soracom-cli.rb
+sed -e "s/SHA256_MAC_amd64 *=.*/SHA256_MAC_amd64 = \"$SHA256_MAC_amd64\"/g" -i'.bak' soracom-cli.rb
+sed -e "s/SHA256_MAC_arm64 *=.*/SHA256_MAC_arm64 = \"$SHA256_MAC_arm64\"/g" -i'.bak' soracom-cli.rb
+sed -e "s/SHA256_LINUX_amd64 *=.*/SHA256_LINUX_amd64 = \"$SHA256_LINUX_amd64\"/g" -i'.bak' soracom-cli.rb
+sed -e "s/SHA256_LINUX_arm64 *=.*/SHA256_LINUX_arm64 = \"$SHA256_LINUX_arm64\"/g" -i'.bak' soracom-cli.rb
+sed -e "s/SHA256_LINUX_386 *=.*/SHA256_LINUX_386 = \"$SHA256_LINUX_386\"/g" -i'.bak' soracom-cli.rb
+sed -e "s/SHA256_LINUX_arm *=.*/SHA256_LINUX_arm = \"$SHA256_LINUX_arm\"/g" -i'.bak' soracom-cli.rb
 
